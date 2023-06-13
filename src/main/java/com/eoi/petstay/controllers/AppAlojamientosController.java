07287afdc35e5b43f0cb9cdc70ae8ac53db29135
Juancho <juancho.rom.pal@gmail.com>
2023-06-14 01:13:55 +0200

    Trabajo de los Alojamientos

--- src/main/java/com/eoi/petstay/controllers/AppAlojamientosController.java
+++ src/main/java/com/eoi/petstay/controllers/AppAlojamientosController.java
@@ -10,19 +10,72 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import com.eoi.petstay.repository.AlojamientoRepository;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
-@RequestMapping("/alojamientos")
 public class AppAlojamientosController {
-    @Autowired
-    private UsuarioService usuarioService;
     private final AlojamientoService alojamientoService;
+    private final UsuarioService usuarioService;
+
     @Autowired
-    public AppAlojamientosController(AlojamientoService alojamientoService) {
+    public AppAlojamientosController(AlojamientoService alojamientoService, UsuarioService usuarioService) {
         this.alojamientoService = alojamientoService;
+        this.usuarioService = usuarioService;
     }
+
+    @GetMapping("/alojamientos/Perfil_Alojamiento")
+    public String perfilAlojamiento() {
+        return "alojamientos/Perfil_Alojamiento";
+    }
+
+    @GetMapping("/alojamientos/Lista_Alojamientos")
+    public String getAllPaginated(@RequestParam(defaultValue = "1") int page,
+                                  @RequestParam(defaultValue = "10") int size,
+                                  @NotNull Model model) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<Alojamientos> alojamientosPage = alojamientoService.getRepo().findAll(pageable);
+
+        model.addAttribute("alojamientos", alojamientosPage);
+
+        int totalPages = alojamientosPage.getTotalPages();
+
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("Alojamientos", alojamientosPage);
+        }
+
+        return "alojamientos/Lista_Alojamientos";
+    }
+
+    @GetMapping("/alojamientos/{id}")
+    public String editar(@PathVariable Long id, Model model) {
+        Optional<Alojamientos> alojamiento = alojamientoService.encuentraPorId(id);
+        model.addAttribute("alojamiento", alojamiento);
+        return "alojamientos/detalles_alojamiento";
+    }
+
+        @GetMapping("/alojamientos/registro_alojamiento")
+        public String registroAlojamiento(Model interfazConPantalla) {
+            // Instancia en memoria del objeto a informar en la pantalla
+            final Alojamientos alojamiento = new Alojamientos();
+
+            interfazConPantalla.addAttribute("datosAlojamiento", alojamiento);
+            System.out.println("Preparando registro de alojamiento");
+            return "alojamientos/registro_alojamiento";
+        }
+
     @GetMapping
     public String obtenerAlojamientos(Model model) {
         List<Alojamientos> alojamientos = alojamientoService.obtenerAlojamientos();
@@ -34,7 +87,7 @@ public class AppAlojamientosController {
         model.addAttribute("alojamiento", new Alojamientos());
         return "alojamientos/registro";
     }
-    @PostMapping("/registro")
+    @PostMapping("alojamientos/registro_alojamiento")
     public String guardarAlojamiento(@ModelAttribute("alojamiento") Alojamientos alojamiento) {
         alojamientoService.guardarAlojamiento(alojamiento);
         return "redirect:/alojamientos";
---
+++ src/main/java/com/eoi/petstay/dto/AlojamientosDto.java
@@ -0,0 +1,20 @@
+package com.eoi.petstay.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class AlojamientosDto {
+
+    private String foto;
+
+    private String nombre;
+
+    private float valoracion;
+}
