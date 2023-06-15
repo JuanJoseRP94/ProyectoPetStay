4ee173b864de65e5cc88f207bae76fa08faa2faa
avalmar <albavalverdemarcos@gmail.com>
2023-06-15 15:27:51 +0200

    Listado en data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -21,3 +21,33 @@ insert into usuarios (
                                           true, 'Apell1', 'Apell2', 'admin@petstay.com', NULL, 'Alba', '$2a$10$GM78bWCZHQF8xnUqh.DyNOdjc6hsa/.ssrXBozbzgiJGslO7RljG6', NULL, NULL, NULL, NULL, '1'
 
                                       );
+insert into especie(id, nombre_especie) values ('1', 'Perro'),
+('2', 'Gato'),
+('3', 'Conejo'),
+('4', 'Hámster'),
+('5', 'Tortuga'),
+('6', 'Pez'),
+('7', 'Pájaro'),
+('8', 'Lagarto');
+
+insert into comportamientos (id, nombre_comportamientos) values ('1', 'Dócil'),
+('2', 'Obediente'),
+('3', 'Agresivo'),
+('4', 'Cariñoso'),
+('5', 'Asustadizo'),
+('6', 'Desobediente'),
+('7', 'Juguetón'),
+('8', 'Dormilón');
+
+insert into sexo (id, descripcion) values ('1', 'Hembra'), ('2', 'Macho');
+
+insert into tamanio (id, nombre_tamanio) values ('1', 'Pequeño'), ('2', 'Mediano'), ('3', 'Grande');
+
+insert into tipo_cuidados (id, descripcion, nombre) values ('1', 'Alimentar 1 vez al día', 'Alimentación'),
+('2', 'Alimentar 2 veces al día', 'Alimentación'),
+('3', 'Pasear 2 veces al día', 'Paseo'),
+('4', 'Pasear 3 veces al día', 'Paseo'),
+('5', 'Bañar', 'Bañar'),
+('6', 'Limpiar arenero', 'Limpieza'),
+('7', 'Limpiar jaula', 'Limpieza'),
+('8', 'Peinar 1 vez al día', 'Peinado');
