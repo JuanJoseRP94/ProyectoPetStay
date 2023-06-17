66514989c2f2655721a5e5eef5dd6338ecffa790
AleSaponi17 <119517949+AleSaponi17@users.noreply.github.com>
2023-06-17 12:07:08 +0200

    Add files via upload

---
+++ src/main/java/com/eoi/petstay/repository/SexoUsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.eoi.petstay.repository;
+
+import com.eoi.petstay.model.SexoUsuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SexoUsuarioRepository extends JpaRepository<SexoUsuario, Long>{
+}
