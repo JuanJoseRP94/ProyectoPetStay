f7c55bb3adcd229e9833457165b6c7d5fdac98af
AleSaponi17 <119517949+AleSaponi17@users.noreply.github.com>
2023-06-17 12:08:32 +0200

    Add files via upload

---
+++ src/main/java/com/eoi/petstay/dto/EventoDto.java
@@ -0,0 +1,24 @@
+package com.eoi.petstay.dto;
+
+import com.eoi.petstay.model.Comportamientos;
+import com.eoi.petstay.model.TipoCuidados;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventoDto {
+    private Long id;
+    private String titulo;
+    private LocalDateTime fechaInicio;
+    private LocalDateTime fechaFin;
+}
